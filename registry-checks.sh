1e9136a14c792d4e33b1bda0365ebce0d5e3c1b1
Matt Borja <me@mattborja.dev>
2025-03-14 08:52:00 +0000

    Promote /policy/bin to /bin and refactor

--- registry-checks.sh
+++ registry-checks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BASH_BIN="/bin/bash"
 REGISTRY_DIR="./registry"
-POLICY_BIN="./policy/bin"
+POLICY_BIN="./bin"
 
 for JSON_FILE in "${REGISTRY_DIR}"/*.json; do
     for CHECK_SCRIPT in "${POLICY_BIN}"/*.sh; do
