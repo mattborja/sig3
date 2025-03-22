1c1612ab93a02d3d5eea48ce151985524f197623
Matt Borja <me@mattborja.dev>
2025-03-22 22:54:58 +0000

    Implement basic ID Lookup with dist/
    Attempts to render selectively sorted JSON using HighlightJS linked from CDN using SRI.

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -1,10 +1,40 @@
 (function (){
     const $form = $('form#registry-search');
     const $query = $form.find('input');
+    const $pre = $('#registry-entry-details pre > code');
+
+    const $modal = new bootstrap.Modal('#registry-entry-details');
 
     $form.submit(e => {
         e.preventDefault();
 
-        alert('Function temporarily unavailable. Please try again later.');
+        const fpr = $query.val().toUpperCase();
+        const src = `dist/${fpr}.json`;
+
+        fetch(src)
+            .then(res => {
+              if (!res.ok)
+                throw res.statusText
+
+              return res.json();
+            })
+            .then(json => {
+                const sorted = {
+                  id: json.id,
+                  valid: json.valid,
+                  status: json.status,
+                  source: json.source,
+                  ...json
+                };
+
+                const formatted = JSON.stringify(sorted, null, 2);
+                
+                $pre.html(formatted);
+                
+                $modal.show();
+            })
+            .catch(e => {
+                alert(e);
+            });
     });
 })();
