8aee74fa18b705efa8f99bd7ae5c3b028439764d
Matt Borja <me@mattborja.dev>
2025-03-22 17:35:38 +0000

    Refactor remaining policies into builder (index.js) - bin/policy/keyversion.sh - bin/policy/filename.sh

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ const REGISTRY_BASE = './registry';
 const REGISTRY_EXT = '.json';
 const ARTIFACT_BASE = process.argv[2] || './dist/';
 
+const NIST_SHA1_WITHDRAWAL_DATE = "2030-12-31T00:00:00Z";
+
+const VALID_KEY_VERS = {
+    128: {
+        ver: 3,
+        deprecated: true,
+    },
+
+    160: {
+        ver: 4,
+        deprecated: Date.now() > Date.parse(NIST_SHA1_WITHDRAWAL_DATE),
+    },
+
+    256: {
+        ver: 6,
+        deprecated: false,
+    },
+};
+
 const validator = new Validator(schema);
 
 // block on output dir creation
@@ -19,9 +38,9 @@ fs.readdir(REGISTRY_BASE, (err, files) => {
     if (err)
         throw err;
 
-    files.forEach(f => {
-        const inFilename = path.join(REGISTRY_BASE, f);
-        const outFilename = path.join(ARTIFACT_BASE, f);
+    files.forEach(baseFilename => {
+        const inFilename = path.join(REGISTRY_BASE, baseFilename);
+        const outFilename = path.join(ARTIFACT_BASE, baseFilename);
 
         if (path.extname(inFilename) !== REGISTRY_EXT)
             return console.warn(`Skipping extraneous file: ${inFilename}`);
@@ -41,15 +60,6 @@ fs.readdir(REGISTRY_BASE, (err, files) => {
                 try
                 {
                     entry.source = JSON.parse(json);
-                }
-                catch (e)
-                {
-                    return console.warn(`Skipping file on parse failure: ${inFilename} (${e})`);
-                }
-
-                // entry.status.schema
-                try
-                {
                     entry.status.schema = validator.validate(entry.source);
                 }
                 catch (e)
@@ -58,11 +68,28 @@ fs.readdir(REGISTRY_BASE, (err, files) => {
                 }
 
                 // entry.status.keyVersion
+                // Checks the key fingerprint length from a JSON file and determines its corresponding key version.
+                // If the key length matches a deprecated version (e.g., 128-bit for version 3), a warning is displayed.
+                //
+                // Please note:
+                // - As of July 2024, RFC9580 §5.5.4.1 specifies that "version 3 (128-bit) keys and MD5 are deprecated" (https://datatracker.ietf.org/doc/html/rfc9580#section-5.5.4.1)
+                // - As of December 2022, NIST has retired SHA-1 with an expected full withdrawal date of December 31, 2030 (https://www.nist.gov/news-events/news/2022/12/nist-retires-sha-1-cryptographic-algorithm)
                 try
                 {
+                    const keyLenBits = 8 * ((entry.source.fingerprint || '').length / 2);
+                    const validKeyVersion = (keyLenBits in VALID_KEY_VERS);
+                    const validKeyDeprecated = validKeyVersion && VALID_KEY_VERS[keyLenBits].deprecated;
+
                     entry.status.keyVersion = {
-                        valid: false,
-                        errors: ['Not implemented']
+                        valid: validKeyVersion && !validKeyDeprecated,
+                        errors:
+                            !validKeyVersion
+                            ? [ `Unrecognized key length: ${keyLenBits} bits` ]
+
+                            : validKeyDeprecated
+                            ? [ `Deprecated key version: ${VALID_KEY_VERS[keyLenBits].ver} (${keyLenBits} bits)` ]
+                            
+                            : []
                     };
                 }
                 catch (e)
@@ -71,29 +98,29 @@ fs.readdir(REGISTRY_BASE, (err, files) => {
                 }
 
                 // entry.status.filename
+                // Checks if the fingerprint from the JSON file matches the expected
+                // filename format: FINGERPRINT.json
                 try
                 {
+                    const expectedFilename = `${entry.source.fingerprint}${REGISTRY_EXT}`;
+                    const isValidFilename = (baseFilename === expectedFilename);
+
                     entry.status.filename = {
-                        valid: false,
-                        errors: ['Not implemented']
+                        valid: isValidFilename,
+                        errors: !isValidFilename ? [ `Expected filename ${expectedFilename}, got ${baseFilename}` ] : []
                     };
                 }
                 catch (e)
                 {
+                    entry.status.filename = {
+                        valid: false,
+                        errors: [ `Filename validation failure: ${e}` ]
+                    };
+
                     return console.warn(`Skipping file on filename validation failure: ${inFilename} (${e})`);
                 }
 
-                // entry.approval
-                try
-                {
-                    entry.approval = {}; // TODO: Summarize final decision
-                }
-                catch (e)
-                {
-                    return console.warn(`Skipping file on final approval failure: ${inFilename} (${e})`);
-                }
-
-                // artifact
+                // build dist artifacts
                 try
                 {
                     // build additional meta (post-validation)
